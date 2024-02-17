8813a231a9900b8a67fa64061831b7e232fe80aa
xuanjian zhang <zwordgize@gmail.com>
2024-02-17 17:02:27 +1100

    find all cashgifts, find one cash gift

--- src/main/java/dev/lancinater/cashGifts/CashGiftController.java
+++ src/main/java/dev/lancinater/cashGifts/CashGiftController.java
@@ -1,13 +1,16 @@
 package dev.lancinater.cashGifts;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/cashGifts")
@@ -19,5 +22,10 @@ public class CashGiftController {
     @GetMapping
     public ResponseEntity<List<CashGift>> getAllCashGifts(){
         return new ResponseEntity<List<CashGift>>(cashGiftService.allCashGifts(),HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<CashGift>> getCashGiftById(@PathVariable ObjectId id){
+        return new ResponseEntity<Optional<CashGift>>(cashGiftService.getOneMovie(id),HttpStatus.OK);
     }
 }
--- src/main/java/dev/lancinater/cashGifts/CashGiftService.java
+++ src/main/java/dev/lancinater/cashGifts/CashGiftService.java
@@ -1,9 +1,11 @@
 package dev.lancinater.cashGifts;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CashGiftService {
@@ -12,5 +14,8 @@ public class CashGiftService {
     private CashGiftRepository cashGiftRepository;
     public List<CashGift> allCashGifts(){
         return cashGiftRepository.findAll();
+    }
+    public Optional<CashGift> getOneMovie(ObjectId id){
+        return cashGiftRepository.findById(id);
     }
 }
